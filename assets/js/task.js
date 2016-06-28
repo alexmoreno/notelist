b52151b34fc7a6f18eaf125898200f1e2c31a5d8
Alex Moreno <alexmorenodealmeida@gmail.com>
2016-06-28 16:03:04 -0300

    Várias funcionalidades novas ae

--- assets/js/task.js
+++ assets/js/task.js
@@ -1,6 +1,10 @@
 // aquele ES6zinho maroto
 'use strict';
-angular.module('taskApp', ['gulp-angular-modules'])
+angular.module('taskApp', [
+    'gulp-angular-modules',
+    "ngSanitize",
+    
+    ])
 
 // config tudo junto aqui mesmo pois é só isso
 .config((localStorageServiceProvider) => {
@@ -11,10 +15,11 @@ angular.module('taskApp', ['gulp-angular-modules'])
 .controller('TasksController', ($scope, localStorageService) => {
     let lss = localStorageService;
     $scope.tasks = [];
-    $scope.tasks.push({title: 'Titulo', text: 'Escreva algo aqui', focused: false})
+    $scope.tasks.push({title: 'Título', text: 'Escreva algo aqui', focused: false})
     
     if(lss.get('tasks')){
         $scope.tasks = lss.get('tasks')
+        console.log($scope.tasks);
         for (let a in $scope.tasks) {
             if($scope.tasks[a].focused == true) $scope.taskOnNote = $scope.tasks[a]
         }
@@ -27,16 +32,28 @@ angular.module('taskApp', ['gulp-angular-modules'])
     
     $scope.focus = (index) => {
         $scope.taskOnNote = $scope.tasks[index]
+        $scope.taskOnNote.options = {
+            controls: ["bold", "italic", "separator", "preview"]
+        };
+
         for (let a in $scope.tasks) {
             $scope.tasks[a].focused = false
         }
-        $scope.tasks[index].focused = true
+        $scope.tasks[index].focused = true  
+        
     }
 
     $scope.delete = (index) => { $scope.tasks.splice(index, 1)  }
 
-    $scope.focus(0)
-
+    let firstTimeAccessed = function(){
+        for (let a in $scope.tasks) {
+            if($scope.tasks[a].focused == true) return true
+        }
+        return false    
+    }
+    if (!firstTimeAccessed()) {
+        $scope.focus(0)
+    }
     $scope.$watch('tasks', () => { lss.set('tasks', $scope.tasks) }, true);
 
 });
--- index.php
+++ index.php
@@ -3,43 +3,61 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  	<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/angular_material/1.1.0-rc2/angular-material.min.css">
 	<title>Notepad da alegria</title>
 	<link rel="stylesheet" href="./assets/css/style.css">
 	
-	 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.js"></script>
-	 <script src="node_modules/angular-local-storage/dist/angular-local-storage.js"></script>
-	 <script src="app/src/init/gulp-angular-modules.js"></script>
-	 <script src="assets/js/task.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.3/angular.js"></script>
+	
+	<script src="node_modules/angular-local-storage/dist/angular-local-storage.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/angular_material/1.1.0-rc2/angular-material.min.js"></script>
+	<script src="app/src/init/gulp-angular-modules.js"></script>
+	
+	
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
+	<script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
+	<script src="bower_components/angular-sanitize/angular-sanitize.js"></script>
+	<script src="https://use.fontawesome.com/27682896b8.js"></script>
+	<script src="assets/js/task.js"></script>
+
+
+
 	 
 	 <!-- inject:js -->
 	<!-- endinject -->
 </head>
-<body>
+<body >
 
 	<div class="wrapper" ng-controller = 'TasksController '>
 		<div class="list-tasks">
 		<div class="list-tasks-menu">
-			<a href="" ng-click='addTask()'>+ Task</a>
+			<div class="list-task-title">Tasks</div>
+			<a href="" ng-click='addTask()'><i class="fa fa-plus" aria-hidden="true"></i>
+ Task</a>
+			<span class='task-counter'> {{tasks.length}} tasks</span>
 		</div>
 			<ul>
-				<li ng-repeat="task in tasks">
-					<a href="" ng-click='focus($index)'>{{task.title}}</a>
-					<a href="" ng-click='delete($index)' class='task-delete'>delete</a>
+				<li class='task-listed' ng-repeat="task in tasks" ng-class="{'focused-task': task.focused }"  ng-click='focus($index)'>
+
+					<a href="" class='task-title'>{{task.title}}</a>
+					<a href="" ng-click='delete($index)' class='task-delete task-action'><i class="fa fa-trash" aria-hidden="true"></i></a>
+					<div class="task-excerpt">{{task.text}}</div>
 				</li>
 			</ul>
 		</div>
 		<div class="notepad-container">
 			<div class="notepad-header">
-				<input type="text" ng-model='taskOnNote.title' id='task-title'>
+				<!-- aqui eu vou colocar aqueles bang de markdown lá que vai ficar irado -->
 			</div>
-			<textarea id="notepad" ng-model='taskOnNote.text' ></textarea>
+			<input type="text" ng-model='taskOnNote.title' id='notepad-title'>
+			
+			<textarea id="notepad" ng-model='taskOnNote.text' ></textarea>			
+
 			
 		</div>
 	</div>
 
-	<script>
-
-
-	</script>
 </body>
 </html>
